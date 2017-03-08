4f829a82f85c14cdaa1a771a0f9b199a2b458cdf
Rhea Parekh <rheaparekh12@gmail.com>
2017-03-08 17:15:39 +0530

    Feed set up

--- CommonFeedPage.php
+++ CommonFeedPage.php
@@ -6,13 +6,21 @@
 <?php
 include 'session.php';
 $comment=$username1=$timeStamp="";
-include 'gitIgnore.php';
-if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    $comment= test_input($_POST["comment_user"]);
-  }
-$timeStamp=date("Y-m-d H:i:s");
 $username1=$_SESSION['login_user'];
-// $sql="INSERT INTO rhea_feed(Username,comment,timeStamp) VALUES ('$username1','$comment','$timestamp')";
+
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    include 'gitIgnore.php';
+    $comment= test_input($_POST["comment_user"]);
+    $timeStamp=date('Y-m-d H:m:s');
+    $sql="INSERT INTO rhea_feed(Username,comment,timeStamp) VALUES ('$username1','$comment','$timeStamp');";
+
+    if($conn->query($sql) === TRUE){
+             echo "success";
+    } else {
+        echo "error: " . $conn->error;  
+    }$conn->close();
+    }
+
 
 function test_input($data) {
      $data = trim($data);
@@ -25,14 +33,35 @@ function test_input($data) {
 <body>
    <h1> Feed Page  </h1>
     <a href="profile.php">  Profile  </a>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER ["PHP_SELF"]); ?>">
+    &emsp;
+    <a href="Logout.php"> Logout </a><br> <br>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
     Comment:
     <br>
     <textarea id="comment_user" name="comment_user" cols="100" rows="5"> </textarea>
     <br> <br>
     <input type="submit" name="submit" value="submit">
-
 </form>
+
+
+<?php 
+include 'gitIgnore.php';
+$sql1="SELECT Username,comment,timeStamp FROM rhea_feed";
+$result1=$conn->query($sql1);
+
+if($result1->num_rows >0){
+      echo"<table> <tr> <th> time </th><th> username </th><th>comment</th></tr>";
+      while($row=$result1->fetch_assoc()){
+          echo "<tr><td>" . $row["timeStamp"]."</td><td>".$row["Username"]."</td><td>".$row["comment"]."</td></tr>";
+      }
+      echo "</table>";
+ } else{
+   echo "no results";
+ }
+
+$conn->close();
+
+?>
 </body>
 
 
