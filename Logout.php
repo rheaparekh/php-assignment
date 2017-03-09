be42bdf61e3859881f74f70a4faf5e8adff688a6
Rhea Parekh <rheaparekh12@gmail.com>
2017-03-09 21:15:26 +0530

    logout cookie clear bug fixed

--- Logout.php
+++ Logout.php
@@ -2,7 +2,7 @@
   session_start();
   $cookie_name="randomkey";
   $cookie_value="key";
-  setcookie($cookie_name,$cookie_value,time() -3600);
+  setcookie($cookie_name,$cookie_value,time() -3600,"/");
 
   if(session_destroy()){
     header("Location: login.php");
